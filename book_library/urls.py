4a44ad8ac3b656e2800465a8b8ace6279c8940c3
ShaneZD <146131214+ShaneZD@users.noreply.github.com>
2024-08-26 17:55:29 +0100

    added initial files

---
+++ book_library/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views  # Make sure this import exists
+
+from django.contrib import admin
+from django.urls import path, include
+from book_library import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Home page
+    path('admin/', admin.site.urls),
+    path('books/', include('books.urls')),  # Book-related URLs
+    path('reviews/', include('reviews.urls')),  # Review-related URLs
+    path('messages/', include('messaging.urls')),  # Messaging-related URLs
+    path('accounts/', include('allauth.urls')),  # Authentication-related URLs (e.g., login, signup)
+    path('users/', include('users.urls')),  # User-related URLs
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
